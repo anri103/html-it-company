1460761f1dbe6b64a61cac59468508ac1dd0f7ce
anri103 <ianrianri@gmail.com>
2023-02-09 18:56:42 +0300

    content add

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ Version: 1.0
 
 window.addEventListener('DOMContentLoaded', event => {
 
+    //////////////////////////////////////////////////////////////////
+    // [ Слайдер на странице index.html ]
+
     const heroSwiper = new Swiper('.heroSwiper', {
         slidesPerView: 1,
         spaceBetween: 24,
@@ -29,6 +32,9 @@ window.addEventListener('DOMContentLoaded', event => {
         },
     });
 
+    //////////////////////////////////////////////////////////////////
+    // [ Слайдер на странице index.html ]
+
     const portfolioSwiper = new Swiper('.portfolioSwiper', {
         slidesPerView: 1.15,
         spaceBetween: 12,
@@ -44,13 +50,45 @@ window.addEventListener('DOMContentLoaded', event => {
             clickable: true,
         },
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '#portfolioSwiper-next',
+            prevEl: '#portfolioSwiper-prev',
         },
         breakpoints: {
             768: {
                 slidesPerView: 2,
                 spaceBetween: 24,
+            },
+        },
+    });
+
+    //////////////////////////////////////////////////////////////////
+    // [ Слайдер на странице blog-item.html ]
+
+    const relatedPosts = new Swiper('.relatedPosts', {
+        slidesPerView: 1,
+        loop: true,
+        grabCursor: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '#relatedPosts-next',
+            prevEl: '#relatedPosts-prev',
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 4,
             },
         },
     });
